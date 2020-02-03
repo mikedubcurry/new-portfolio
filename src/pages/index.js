d5c8d77b42ac0678a25d51b7621fea0d52255e98
michaelcurry95 <fender18295@gmail.com>
2020-02-03 17:49:59 -0500

    topics state is set by a double-looped useEffect. seek a better solution

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -6,7 +6,28 @@ import SEO from "../components/seo"
 import Panel from "../components/Panel"
 
 export default function Index({ data }) {
-  // console.log(data)
+  const [topics, setTopics] = useState([])
+  const [selected, setSelected] = useState("")
+
+  const repos = data.githubData.data.user.pinnedRepositories.edges
+
+  useEffect(() => {
+    // go through each repo
+    if (repos) {
+      repos.forEach(({ node }) => {
+        // in each repo, go through its topics
+        node.topics.edges.forEach(topic => {
+          if (!topics.includes(topic.node.topic.name)) {
+            setTopics([...topics, topic.node.topic.name])
+          }
+        })
+      })
+    }
+    // a double nested loop repeated topics.length times.
+    // done only because repos.length <= 6, and each repo has at most 3 topics.
+    // worst case is 216 renders
+    // there has to be a better way
+  }, [topics])
   return (
     <Layout>
       <SEO title="HOME" />
