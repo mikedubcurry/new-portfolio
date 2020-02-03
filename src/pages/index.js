14f38829d031a9937be07d34ae906a7a8202e06c
michaelcurry95 <fender18295@gmail.com>
2020-02-03 17:05:35 -0500

    index page query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Panel from "../components/Panel"
 
-export default () => (
-  <Layout>
-    <SEO title="HOME" />
-    <Panel pos={"left"} ct={2}></Panel>
-    <Panel pos={"right"} ct={2}></Panel>
-  </Layout>
-)
+export default function Index({ data }) {
+  // console.log(data)
+  return (
+    <Layout>
+      <SEO title="HOME" />
+      <Panel pos={"left"} ct={2}></Panel>
+      <Panel pos={"right"} ct={2}></Panel>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query IndexPageQuery {
+    githubData {
+      data {
+        user {
+          pinnedRepositories {
+            edges {
+              node {
+                name
+                description
+                homepageUrl
+                url
+                topics: repositoryTopics {
+                  edges {
+                    node {
+                      topic {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
