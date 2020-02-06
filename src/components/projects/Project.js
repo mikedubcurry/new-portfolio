e239aba6eb7ec877616fa84adcd1d3fc672fce2f
michaelcurry95 <fender18295@gmail.com>
2020-02-05 20:50:39 -0500

    data is there, need styles

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -7,9 +7,22 @@ const ProjectView = styled.div``
 
 export default function Project({ repo }) {
   const { name, description, homepageUrl, url, topics } = repo
+
+  const shortedText = str => {
+    if(!str) {
+      return ''
+    }
+    if(str.split(' ').length < 10)
+    {
+      return str
+    }
+    return `${str.split(' ').slice(0,9).join(' ')}...`
+  }
+  
   return (
     <ProjectView>
       <h3>{formatTopic(name)}</h3>
+      <p>{shortedText(description)}</p>
     </ProjectView>
   )
 }
