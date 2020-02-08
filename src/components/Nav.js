9377173fb5918dcf23391c9345a36305d1654e02
michaelcurry95 <fender18295@gmail.com>
2020-02-08 13:35:12 -0500

    mobil nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,53 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 
+const BurgerBtn = styled.div`
+  outline: none;
+  width: 3rem;
+  height: 3rem;
+  position: absolute;
+  z-index: 5;
+  top: 1rem;
+  right: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .top,
+  .mid,
+  .bot {
+    height: 0.6rem;
+    background: #222;
+    transition: transform 0.4s, opacity 0.2s;
+    border-radius: 4rem;
+    opacity: 1;
+  }
+
+  .top.open {
+    transform: rotate(405deg) translate(0.82rem, 0.82rem);
+  }
+  .mid.open {
+    opacity: 0;
+  }
+  .bot.open {
+    transform: rotate(315deg) translate(0.82rem, -0.82rem);
+  }
+
+  @media screen and (min-width: 630px) {
+    display: none;
+  }
+`
+
 const NavBar = styled.nav`
-  width: 50%;
   display: flex;
   justify-content: space-between;
-  position: absolute;
-  z-index: 3;
-  left: 25%;
-  top: 5%;
+  width: 100%;
+  position: unset;
+  flex-direction: column;
+  transform: translateY(-100%);
+
   a {
     font-weight: 600;
     color: white;
@@ -20,48 +58,72 @@ const NavBar = styled.nav`
     transition: all 0.3s;
 
     &.current {
-      color: yellow;
+      color: darkblue;
     }
 
-    &:hover {
-      transform: translateY(-45%);
-    }
     &:active {
-      transform: translateY(-33%);
+      color: orange;
     }
   }
-  @media screen and (max-width: 630px) {
-    width: 100%;
-    position: unset;
-    flex-direction: column;
 
-    &.current {
-      color: yellow;
-    }
+  @media screen and (min-width: 630px) {
+    transform: none;
+    width: 50%;
+    flex-direction: row;
+    position: absolute;
+    z-index: 3;
+    left: 25%;
+    top: 5%;
+    a {
+      font-weight: 600;
+      color: white;
+      text-decoration: none;
+      font-size: 2rem;
+      transform: translateY(-33%);
+      font-family: sans serif;
+      transition: all 0.3s;
 
-    &:hover {
-      transform: none;
-    }
+      &.current {
+        color: yellow;
+      }
 
-    &:active {
-      transform: none;
-      color: orange;
+      &:hover {
+        transform: translateY(-45%);
+      }
+      &:active {
+        transform: translateY(-33%);
+      }
     }
   }
 `
 
 const Nav = () => (
-  <NavBar>
-    <Link activeClassName="current" to="/">
-      PROJECTS
-    </Link>
-    <Link activeClassName="current" to="/resume">
-      RESUME
-    </Link>
-    <Link activeClassName="current" to="/contact">
-      CONTACT
-    </Link>
-  </NavBar>
+  <>
+    <MenuBtn />
+    <NavBar>
+      <Link activeClassName="current" to="/">
+        PROJECTS
+      </Link>
+      <Link activeClassName="current" to="/resume">
+        RESUME
+      </Link>
+      <Link activeClassName="current" to="/contact">
+        CONTACT
+      </Link>
+    </NavBar>
+  </>
 )
 
 export default Nav
+
+function MenuBtn() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <BurgerBtn tabIndex={0} onClick={() => setOpen(!open)}>
+      <div className={`top ${open ? "open" : ""}`}></div>
+      <div className={`mid ${open ? "open" : ""}`}></div>
+      <div className={`bot ${open ? "open" : ""}`}></div>
+    </BurgerBtn>
+  )
+}
