0479760b73fe023b116f3cfb2f090805ebcc9335
michaelcurry95 <fender18295@gmail.com>
2020-02-11 19:15:29 -0500

    Projects link to repos

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,29 +1,41 @@
 import React from "react"
 import styled from "styled-components"
 
-import {formatTopic} from './TopicList'
+import { formatTopic } from "./TopicList"
 
 const ProjectView = styled.div`
-font-family: 'Roboto';
+  font-family: "Roboto";
+
+  a {
+    color: unset;
+    cursor: pointer;
+    color: #353535;
+  }
 `
 
 export default function Project({ repo }) {
-  const { name, description } = repo
+  const { name, description, url } = repo
 
   const shortedText = str => {
-    if(!str) {
-      return ''
+    if (!str) {
+      return ""
     }
-    if(str.split(' ').length < 10)
-    {
+    if (str.split(" ").length < 10) {
       return str
     }
-    return `${str.split(' ').slice(0,9).join(' ')}...`
+    return `${str
+      .split(" ")
+      .slice(0, 9)
+      .join(" ")}...`
   }
-  
+
   return (
     <ProjectView>
-      <h3>{formatTopic(name)}</h3>
+      <h3>
+        <a target="_blank" href={url}>
+          {formatTopic(name)}
+        </a>
+      </h3>
       <p>{shortedText(description)}</p>
     </ProjectView>
   )
